21f0e7dfa521486896f04934172ffc9dfca1e926
hustcc <i@atool.org>
2016-11-29 11:27:57 +0800

    remove debug mode

--- run_webhook.py
+++ run_webhook.py
@@ -9,4 +9,4 @@ Created on 2016年10月20日
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=18340, debug=True)
+    socketio.run(app, host='0.0.0.0', port=18340)
